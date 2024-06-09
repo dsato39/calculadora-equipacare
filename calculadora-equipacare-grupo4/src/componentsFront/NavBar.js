71206c43304db1d2b5f556bee6854b3dfe66bb08
NATAN ASSIS <167131823+NatanASSIS99@users.noreply.github.com>
2024-06-09 20:08:46 -0300

    inserindo a logo

--- calculadora-equipacare-grupo4/src/componentsFront/NavBar.js
+++ calculadora-equipacare-grupo4/src/componentsFront/NavBar.js
@@ -1,12 +1,17 @@
-import style from './navbar.module.css'
-const Navbar =() => {
-    return(
-        <div className={style.navbar}>
-            <h1>Título</h1>
-            <p>Subtítulo</p>
+import React from 'react';
+import logo from '../Imagens/logoCompleto.svg';
+import iconeFace from '../Imagens/Icones/iconeFace.svg';
+import style from './navbar.module.css';
 
+const Navbar = () => {
+    return (
+        <div className={style.navbar}>
+            <img src={logo} alt="LogoEquipacare" className={style.logo} />
+            <img className={style.logoFace} src={iconeFace} alt="LogoFace" />
+            <p>Subtítulo</p>
+            <p>Home</p>
         </div>
-    )
+    );
 };
 
 export default Navbar;
